8c41179f87f46a9d349c9e38b7a8f9557955b399
davide--bocc <davide.bocc@gmail.com>
2025-08-15 20:26:11 +0200

    Agg APK Debug del 15/08/2025

--- android/app/src/main/kotlin/fit/app/mobile/MainApplication.kt
+++ android/app/src/main/kotlin/fit/app/mobile/MainApplication.kt
@@ -9,7 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.swmansion.reanimated.ReanimatedJSIModulePackage
+import com.facebook.react.PackageList
 
 class MainApplication : Application(), ReactApplication {
 
@@ -26,12 +26,7 @@ class MainApplication : Application(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-
-        override fun getJSIModulePackage() = ReanimatedJSIModulePackage()
       }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(this, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
