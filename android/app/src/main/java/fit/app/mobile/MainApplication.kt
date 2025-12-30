6e26c28b3667e93a4396b7a1f19c09b8f5956d90
davide--bocc <davide.bocc@gmail.com>
2025-12-30 11:15:02 +0100

    Agg. 30/12/2025

--- android/app/src/main/java/fit/app/mobile/MainApplication.kt
+++ android/app/src/main/java/fit/app/mobile/MainApplication.kt
@@ -4,24 +4,30 @@ import android.app.Application
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-    override val reactNativeHost: ReactNativeHost =
-        object : DefaultReactNativeHost(this) {
-            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-            override fun getJSMainModuleName(): String = "index"
-
-            override fun getPackages(): MutableList<ReactPackage> {
-                // Ritorna una lista vuota oppure aggiungi manualmente i pacchetti se vuoi
-                return mutableListOf()
-            }
+    override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> {
+            return emptyList()
         }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+    }
 
     override fun onCreate() {
         super.onCreate()
         SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            load()
+        }
     }
 }
