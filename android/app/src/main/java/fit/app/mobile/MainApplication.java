10f285888a436bb7516dec1f6581aa120edc8864
davide--bocc <davide.bocc@gmail.com>
2026-01-05 18:32:56 +0100

    Agg. 05/01/2026

--- android/app/src/main/java/fit/app/mobile/MainApplication.java
+++ android/app/src/main/java/fit/app/mobile/MainApplication.java
@@ -4,18 +4,27 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
     private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
 
         @Override
         protected List<ReactPackage> getPackages() {
-            return new PackageList(this).getPackages();
+            return new ArrayList<>();
         }
+
 
         @Override
         protected String getJSMainModuleName() {
@@ -28,8 +37,8 @@ public class MainApplication extends Application implements ReactApplication {
         }
 
         @Override
-        protected boolean isHermesEnabled() {
-            return BuildConfig.IS_HERMES_ENABLED;
+        protected Boolean isHermesEnabled() {
+            return true;
         }
     };
 
@@ -41,9 +50,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
+        SoLoader.init(this, false);
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load();
+            DefaultNewArchitectureEntryPoint.load();
         }
     }
 }
