dc4690196f1854937b170488e1bcdc404eeaf929
davide--bocc <davide.bocc@gmail.com>
2026-01-08 18:15:25 +0100

    Agg. 08/01/2026

--- android/app/src/main/java/fit/app/mobile/MainApplication.java
+++ android/app/src/main/java/fit/app/mobile/MainApplication.java
@@ -1,46 +1,55 @@
 package fit.app.mobile;
 
 import android.app.Application;
+
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 
-import java.util.List;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import java.util.ArrayList;
-
+import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new DefaultReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
+    private final ReactNativeHost mReactNativeHost =
+            new DefaultReactNativeHost(this) {
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return new ArrayList<>();
-        }
+                // Abilita il supporto developer in debug
+                @Override
+                public boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
+
+                @Override
+                public List<ReactPackage> getPackages() {
+                    List<ReactPackage> packages = new ArrayList<>();
+                    packages.add(new MainReactPackage());
+                    return packages;
+                }
 
 
-        @Override
-        protected String getJSMainModuleName() {
-            return "index";
-        }
+                // Entry point JS
+                @Override
+                protected String getJSMainModuleName() {
+                    return "index";
+                }
 
-        @Override
-        protected boolean isNewArchEnabled() {
-            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
+                // Nuova architettura
+                @Override
+                protected boolean isNewArchEnabled() {
+                    return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+                }
 
-        @Override
-        protected Boolean isHermesEnabled() {
-            return true;
-        }
-    };
+                // Abilita Hermes
+                @Override
+                protected Boolean isHermesEnabled() {
+                    return true;
+                }
+            };
 
     @Override
     public ReactNativeHost getReactNativeHost() {
@@ -50,7 +59,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        SoLoader.init(this, false);
+        SoLoader.init(this, /* native exopackage */ false);
+
+        // Carica entry point nuova architettura se abilitata
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             DefaultNewArchitectureEntryPoint.load();
         }
