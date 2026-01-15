d0476db839dbda5df9655441694ffbab794ef217
davide--bocc <davide.bocc@gmail.com>
2026-01-15 11:24:20 +0100

    Agg 15/01/2026

--- android/app/src/main/java/fit/app/mobile/MainApplication.java
+++ android/app/src/main/java/fit/app/mobile/MainApplication.java
@@ -4,13 +4,11 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -18,30 +16,26 @@ public class MainApplication extends Application implements ReactApplication {
     private final ReactNativeHost mReactNativeHost =
             new DefaultReactNativeHost(this) {
 
-                // Abilita il supporto developer in debug
                 @Override
                 public boolean getUseDeveloperSupport() {
                     return BuildConfig.DEBUG;
                 }
 
                 @Override
-                public List<ReactPackage> getPackages() {
-                    return new PackageList(this).getPackages();
+                protected List<ReactPackage> getPackages() {
+                    return new java.util.ArrayList<>();
                 }
 
-                // Entry point JS
                 @Override
                 protected String getJSMainModuleName() {
                     return "index";
                 }
 
-                // Nuova architettura
                 @Override
                 protected boolean isNewArchEnabled() {
                     return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
                 }
 
-                // Abilita Hermes
                 @Override
                 protected Boolean isHermesEnabled() {
                     return true;
@@ -56,9 +50,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
+        SoLoader.init(this, false);
 
-        // Carica entry point nuova architettura se abilitata
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             DefaultNewArchitectureEntryPoint.load();
         }
--- android/app/src/main/kotlin/fit/app/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/fit/app/mobile/MainActivity.kt
@@ -2,18 +2,17 @@ package fit.app.mobile
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.ReactRootView
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
     override fun getMainComponentName(): String = "FitAppMobile"
 
     override fun createReactActivityDelegate(): ReactActivityDelegate {
-        return object : ReactActivityDelegate(this, mainComponentName) {
-            override fun createRootView(): ReactRootView {
-                return RNGestureHandlerEnabledRootView(this@MainActivity)
-            }
-        }
+        return DefaultReactActivityDelegate(
+            this,
+            mainComponentName,
+            BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        )
     }
 }
